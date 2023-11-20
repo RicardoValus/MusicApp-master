d927a4e201506dbd0f156b46107e983c2d98be89
RicardoValus <ricardovalus@gmail.com>
2023-11-20 16:06:09 -0300

    Logout inserido

--- src/app/view/musica/home/home.page.ts
+++ src/app/view/musica/home/home.page.ts
@@ -5,38 +5,42 @@ import { Musica } from 'src/app/model/entities/Musica';
 import { AutenticacaoService } from 'src/app/model/services/autenticacao.service';
 import { FirebaseService } from 'src/app/model/services/firebase.service';
 
-
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  public listaDeMusicas : Musica[] = [];
+  public listaDeMusicas: Musica[] = [];
 
-  constructor(private alertController : AlertController,
-    private router : Router, private firebase : FirebaseService, private auth: AutenticacaoService) {
-    console.log(this.auth.getUserLogged())
-    this.firebase.buscarTodos() 
-    .subscribe(res => {
-      this.listaDeMusicas = res.map(musica => {
-        return{
+  constructor(
+    private alertController: AlertController,
+    private router: Router,
+    private firebase: FirebaseService,
+    private auth: AutenticacaoService
+  ) {
+    console.log(this.auth.getUserLogged());
+    this.firebase.buscarTodos().subscribe((res) => {
+      this.listaDeMusicas = res.map((musica) => {
+        return {
           id: musica.payload.doc.id,
-            ...musica.payload.doc.data() as any
-      }as Musica
-    })
-    })
-    }
-  
-  irParaCadastrar(){
-    this.router.navigate(["/cadastrar"]);
+          ...musica.payload.doc.data() as any,
+        } as Musica;
+      });
+    });
   }
 
-  editar(musica : Musica){
-    this.router.navigateByUrl("/detalhar", {state : {musica : musica}});
+  irParaCadastrar() {
+    this.router.navigate(['/cadastrar']);
   }
 
+  editar(musica: Musica) {
+    this.router.navigateByUrl('/detalhar', { state: { musica: musica } });
+  }
+
+  deslogar() {
+    this.auth.logOut().then(() => {
+      this.router.navigate(['/logar']);
+    });
+  }
 }
-
-
-
